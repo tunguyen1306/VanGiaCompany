8a42f895c513d8e7cd82a122a67a46ade7f3689a
Thien <ducthienweakt@gmail.com>
2017-05-08 17:31:11 +0700

    no message

--- WebVanGia/WebVanGia/Controllers/DefaultController.cs
+++ WebVanGia/WebVanGia/Controllers/DefaultController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebVanGia.Models;
 
 namespace WebVanGia.Controllers
 {
     public class DefaultController : Controller
     {
+        private DataAdminVanGiaEntities dbadmin = new DataAdminVanGiaEntities();
         //
         // GET: /Default/
 
@@ -15,6 +17,22 @@ namespace WebVanGia.Controllers
         {
             return View();
         }
-
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult BlogList()
+        {
+            return View();
+        }
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+        public ActionResult DetailBlog(string id)
+        {
+            var id_ = int.Parse(id.Split('-').Last());
+            return View(dbadmin.tbl_blog_tra.Where(x=>x.id_blog_tra== id_).FirstOrDefault());
+        }
     }
 }
